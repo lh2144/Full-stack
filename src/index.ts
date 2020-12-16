4ab75f5a2e55f4dc42f704c63d49e5997709e72d
lh2144 <hlian@stevens.edu>
2020-12-15 21:52:23 -0500

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        dbName: 'redditcopy',
-        type: 'postgresql',
-        debug: !__prod__
+    const orm = await MikroORM.init(mikroConfig);
+
+    const post = orm.em.create(Post, {title: 'my first post'});
+    await orm.em.persistAndFlush(post);
+
+    const app = express();
+    app.get('/', (_, res) => {
+        res.send('hello')
     });
+    app.listen(4000, () => {})
 }
 
 
-main();
+main().catch((err) => console.log(err));

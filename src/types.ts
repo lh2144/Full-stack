d3bbd9d8ec39f9f45d01ca3eb11bffc14c1563a4
lh2144 <hlian@stevens.edu>
2020-12-29 14:54:30 +0800

    add redis

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from 'express';
+import { Session } from 'express-session';
 
 export type MyContext = {
-    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+    req: Request & {session: Session | any};
+    res: Response
 }

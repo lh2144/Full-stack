6ed4327b29caf8001d50dd455bac8faf4b3ff4d8
lian hang <lianhang9393@gmail.com>
2021-02-08 16:50:08 +0800

    changes

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -107,7 +107,7 @@ export class UserResolver {
             return {
                 errors: [
                     {
-                        field: "username",
+                        field: "usernameOrEmail",
                         message: "that username does not exist",
                     },
                 ],

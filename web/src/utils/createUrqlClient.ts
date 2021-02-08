3f065771dd52dc1df3ecbc3576f5eef24bc71a76
lian hang <lianhang9393@gmail.com>
2021-02-08 15:23:34 +0800

    small fix

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -12,8 +12,8 @@ import {
 export const createUrqlClient = (ssrExchange) => ({
     url: "http://localhost:4000/graphql",
     fetchOptions: {
-        credentials: "include",
-    } as const,
+        credentials: "include" as const,
+    },
     exchanges: [
         dedupExchange,
         cacheExchange({

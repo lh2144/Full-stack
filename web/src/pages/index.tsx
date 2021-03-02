dc115783cd89ec0326a5840eab9b2fc082cc9531
lian hang <lianhang9393@gmail.com>
2021-03-02 11:23:21 +0800

    small fix

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,16 +3,35 @@ import { NavBar } from "src/components/navbar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "src/utils/createUrqlClient";
 import { usePostsQuery } from "src/generated/graphqa";
+import { Layout } from "src/components/Layout";
+import { Flex, Stack } from "@chakra-ui/react";
 
 const Index = () => {
-    const [{data}] = usePostsQuery();
-    return (
-        <>
-            <NavBar />
-            <div>Hello worl</div>
-            {!data ? null : data.posts.map(p => <div key={p.id}>{p.title}</div>)}
-        </>
-    );
-};
+    const { data, error, loading, fetchMore, variables } = usePostsQuery({
+        variables: {
+            limit: 15,
+            cursor: null,
+        },
+        notifyOnNetworkStatusChange: true,
+    });
 
-export default withUrqlClient(createUrqlClient, {ssr: true})(Index);
+    if (!loading && !data) {
+        return <div>
+            <div>unknown reason cause error</div>
+            <div>{error?.message}</div>
+        </div>;
+    }
+
+    return (
+        <Layout>
+            {!data && loading ? (<div>Loading...</div>) : (
+                <Stack spacing={8}>
+                    {data!.posts.posts.map((p) => 
+                    !p ? null : (
+                        <Flex></Flex>
+                    ))}
+                </Stack>
+            )}
+        </Layout>
+    )
+};
